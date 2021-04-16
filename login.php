9e4f1df6e94072223c29014c6917c1c83ec7d7ac
rizwanahmadawan <82621603+rizwanahmadawan@users.noreply.github.com>
2021-04-16 17:52:35 +0500

    Removing comments

--- login.php
+++ login.php
@@ -9,7 +9,7 @@
 <body>
   <div class="wrapper">
     <section class="form login">
-      <header>Realtime Chat App</header>
+      <header>PakChat</header>
       <form action="#" method="POST" enctype="multipart/form-data" autocomplete="off">
         <div class="error-text"></div>
         <div class="field input">
--- php/config.php
+++ php/config.php
@@ -1,8 +1,15 @@
 <?php
-  $hostname = "localhost";
+  //Development Purpose
+  /*$hostname = "localhost";
   $username = "root";
   $password = "";
-  $dbname = "chatapp";
+  $dbname = "chatapp";*/
+
+   
+ $hostname = "remotemysql.com";
+  $username = "b160wlRLDe";
+  $password = "NVepohH46D";
+  $dbname = "b160wlRLDe";
 
   $conn = mysqli_connect($hostname, $username, $password, $dbname);
   if(!$conn){
